fa42e156bae9f21c55d6d5a45aa594aa611c55fd
dailycpp <dailyproject2000@gmail.com>
2022-05-24 08:56:51 -0700

    [UPD] Add

--- SIMD/Add.cpp
+++ SIMD/Add.cpp
@@ -1,16 +1,61 @@
 #include "pch.h"
+#include <execution>
+constexpr size_t kSize{100'000'000};
+
 using namespace std;
-class AddingAlgorithmTest : public ::testing::Test {
+
+class AddingAlgorithmTest : public ::testing::Test
+{
 protected:
-    void SetUp() override {
-        vec_.resize(10);
-    }
-    vector<int> vec_{};
+	void PopulateVector()
+	{
+		for (size_t i{}; i < kSize; ++i)
+		{
+			vec_[i] = i;
+		}
+	}
+
+	void SetUp() override
+	{
+		vec_.resize(kSize);
+		PopulateVector();
+	}
+
+	vector<uint64_t> vec_{};
 };
 
-TEST(Add, SequencialForeach)
+TEST_F(AddingAlgorithmTest, UnSequencialForeach)
 {
+	for_each(execution::unseq, begin(vec_), end(vec_),
+		[](uint64_t& data) { ++data; });
 
-  EXPECT_EQ(1, 1);
-  EXPECT_TRUE(true);
+	EXPECT_EQ(1, 1);
+	EXPECT_TRUE(true);
+}
+
+TEST_F(AddingAlgorithmTest, SequencialForeach)
+{
+	for_each(execution::seq, begin(vec_), end(vec_),
+		[](uint64_t& data) { ++data; });
+
+	EXPECT_EQ(1, 1);
+	EXPECT_TRUE(true);
+}
+
+TEST_F(AddingAlgorithmTest, ParallelForeach)
+{
+	for_each(execution::par, begin(vec_), end(vec_),
+		[](uint64_t& data) { ++data; });
+
+	EXPECT_EQ(1, 1);
+	EXPECT_TRUE(true);
+}
+
+TEST_F(AddingAlgorithmTest, ParallelUnsequencialForeach)
+{
+	for_each(execution::par_unseq, begin(vec_), end(vec_),
+		[](uint64_t& data) { ++data; });
+
+	EXPECT_EQ(1, 1);
+	EXPECT_TRUE(true);
 }
